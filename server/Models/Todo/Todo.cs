4f57adc4ddf455a3c11e56f88fe3015ce6c8ee7b
konect9 <sorinbulgari@gmail.com>
2020-12-07 07:15:35 +0200

    Commit1

---
+++ server/Models/Todo/Todo.cs
@@ -0,0 +1,17 @@
+namespace TodoApp.Models
+{
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
+    public class Todo
+    {
+        [BsonId]
+        public ObjectId InternalId { get; set; }
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Body { get; set; }
+        public string Category { get; set; }
+
+    }
+}
